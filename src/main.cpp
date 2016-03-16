82ca7e6f7750c08add9c542ad71eb002d6bfcdec
mammix2 <mammix2@hotmail.com>
2016-03-16 20:30:04 +0000

    extend block time to 5 mins at height 686000

--- src/main.cpp
+++ src/main.cpp
@@ -1114,6 +1114,9 @@ unsigned int static GetNextWorkRequired_legacy(const CBlockIndex* pindexLast)
 
 static unsigned int GetNextTargetRequired_DGW(const CBlockIndex* pindexLast, bool fProofOfStake)
 {
+    if (pindexBest->nHeight > (fTestNet ? 100 : 686000)) {
+        nTargetSpacing = 5 * 60;
+	}
     CBigNum bnTargetLimit = fProofOfStake ? bnProofOfStakeLimit : bnProofOfWorkLimit;
 
     if (pindexLast == NULL)
